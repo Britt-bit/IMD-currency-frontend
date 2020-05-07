2fabb8485579aa93efb1a54fb48b245bd509b097
britt <britt110100@gmail.com>
2020-05-07 18:20:59 +0200

    login with token

---
+++ login.js
@@ -0,0 +1,31 @@
+var btnLogin = document.querySelector(".login button").addEventListener("click", (e) => {
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    console.log("button clicked");
+
+    fetch('http://localhost:3000/api/user_data/login', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+        
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if(json.status === "success"){
+
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "index.html";
+        } else {
+            console.log("fail")
+            let feedback = document.querySelector(".alert");
+            feedback.textContent = "login in failed.";
+            feedback.classList.remove('hidden');
+        }
+    })
+});
