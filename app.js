825cd7c53360e94fb4a02fe3d41b2f1223ccf541
britt <britt110100@gmail.com>
2020-05-07 14:58:27 +0200

    initialized login and signup

---
+++ app.js
@@ -0,0 +1,24 @@
+var btnSignup = document.querySelector(".signup button").addEventListener("click", signup => {
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    //console.log("button clicked");
+
+    fetch('http://localhost:3000/api/user_data/signup', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if(json.status === "success"){
+            let feedback = document.querySelector(".alert");
+            feedback.textContent = "Sign up complete!";
+            feedback.classList.remove('hidden');
+        }
+    })
+});
