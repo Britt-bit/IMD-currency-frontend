d261fd940ea4364b4d3ed29bf8ae12ff36615fab
britt <britt110100@gmail.com>
2020-05-12 09:22:18 +0200

    form to add transfer to database

--- js/transfer.js
+++ js/transfer.js
@@ -1,12 +1,56 @@
+fetch("http://localhost:3000/api/transaction", {
+    'headers': {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+}).then(result => {
+    return result.json();
+}).then(json => {
+    console.log(json);
+}).catch(err => {
+    window.location.href = "login.html";
+});
+
+/* add transaction to database */
+
 var btnTransfer = document.querySelector(".transfer--btn").addEventListener("click", (e) => {
     console.log('clicked');
-    let name = document.querySelector('#name').value;
-    let amount = document.querySelector('#amount').value;
+    let toUser = document.querySelector('#name').value;
+    let coins = document.querySelector('#coins').value;
     let reason = document.querySelector('#reason').value;
     let message = document.querySelector('#message').value;
 
-    console.log(name);
-    console.log(amount);
+    console.log(toUser);
+    console.log(coins);
     console.log(reason);
     console.log(message);
-});
+    
+        fetch("http://localhost:3000/api/transaction", {
+            method: "post",
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+            
+            body: JSON.stringify({
+                "toUser": toUser,
+                "coins": coins,
+                "reason": reason,
+                "message": message
+            })
+        })
+        .then(result => {
+            return result.json();
+        }).then(json => {
+            console.log(json);
+            let transaction = 
+            console.log(json.data._id);
+            console.log(json.data.toUser);
+            console.log(json.data.coins);
+            console.log(json.data.reason);
+                console.log(json.data.message);
+                `<p> Thank you for your transaction</p>`;
+             document.querySelector(".transaction__new").insertAdjacentHTML('afterend', transaction);
+        }).catch(err => {
+            console.log(err)
+        }); e.preventDefault();
+    });
