02f1581f1d63aaef7bde1649d287269afa22f8ca
Baribal172 <vincentvandesompel@hotmail.com>
2020-05-21 19:55:13 +0200

    Start front-end history

---
+++ js/history.js
@@ -0,0 +1,24 @@
+const base_url = "https://imd-coin.herokuapp.com/";
+let toUserID;
+fetch(base_url + "api/transaction", {
+    'headers': {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+}).then(result => {
+    return result.json();
+}).then(json => {
+    console.log(json.data);
+    console.log("werkt");
+}).catch(err => {
+    window.location.href = "login.html";
+});
+
+fetch(base_url + "api/my_user_data/", {
+    'headers': {
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+    }
+}).then(result => {
+    return result.json();
+}).then(json => {
+    console.log(json.data);
+})
