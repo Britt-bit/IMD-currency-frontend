3d58ddc9e78a983ca827d017a27c5630ad2bd959
Baribal172 <vincentvandesompel@hotmail.com>
2020-05-22 16:21:15 +0200

    history live with websockets

--- js/history.js
+++ js/history.js
@@ -1,12 +1,50 @@
 const base_url = "https://imd-coin.herokuapp.com/";
 
+primus = Primus.connect(base_url, {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+            ,
+        min: 500 // Number: The minimum delay before we try reconnect.
+            ,
+        retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+
+primus.on('data', (json) => {
+    if (json.action === "updateHistory") {
+        appendHistory(json.data);
+    }
+});
+
+let appendHistory = (json) => {
+    json.data.data.forEach(element => {
+
+        var newTransfer =
+            `<div class="transfers">
+            <h3>From User:</h3>
+            <p id="fromUser">"${element.fromUser}"</p>
+            
+            <h3>To User: </h3>
+            <p id="toUser">"${element.toUser}"</p>
+    
+            <h3>Amount:</h3>
+            <p id="amountCoins">"${element.coins}" coins</p>
+            
+            <h3>Reason:</h3>
+            <p id="Reason">"${element.reason}" </p>
+            </div>`;
+        document.querySelector(".transfers").insertAdjacentHTML('afterend', newTransfer);
+    });
+
+
+}
+
 fetch(base_url + "api/my_user_data/", {
     method: "get",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + localStorage.getItem('token')
     }
-
 }).then(result => {
     return result.json();
 }).then(json => {
@@ -24,40 +62,11 @@ fetch(base_url + "api/my_user_data/", {
     }).then(result => {
         return result.json();
     }).then(json => {
-        console.log(json);
-        // console.log(json.data.data[0]._id);
-        // let hits = json.data.data.length;
-        // console.log(hits);
-        // const htmlFromUser = document.querySelector("#fromUser");
-        // const htmlToUser = document.querySelector("#toUser");
-        // const htmlCoins = document.querySelector("#amountCoins");
-        // const htmlReason = document.querySelector("#Reason");
-
-        json.data.data.forEach(element => {
-
-            // htmlFromUser.innerHTML = element.fromUser;
-            // htmlToUser.innerHTML = element.toUser;
-            // htmlCoins.innerHTML = element.coins + " coins";
-            // htmlReason.innerHTML = element.reason;
-            var newTransfer =
-                `<div class="transfers">
-                    <h3>From User:</h3>
-                    <p id="fromUser">"${element.fromUser}"</p>
-        
-                    <h3>To User: </h3>
-                    <p id="toUser">"${element.toUser}"</p>
-        
-                    <h3>Amount:</h3>
-                    <p id="amountCoins">"${element.coins}" coins</p>
-        
-                    <h3>Reason:</h3>
-                    <p id="Reason">"${element.reason}" </p>
-                </div>
-                ------------------------------------------------
-                `;
-            document.querySelector(".transfers").insertAdjacentHTML('afterend', newTransfer);
+        primus.write({
+            "action": "updateHistory",
+            "data": json
         });
-
-
+        console.log(json);
+        // appendHistory(json);
     })
 })
